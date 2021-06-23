c51c46e052d4a0c898ddab4926eced17a009065c
ZABBIR <zabbirhossain729@gmail.com>
2021-06-23 16:47:59 +0600

    update

---
+++ database/migrations/2018_02_17_211248_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('sku');
+            $table->text('category');
+            $table->float('price',8,2);
+            $table->text('description');
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
